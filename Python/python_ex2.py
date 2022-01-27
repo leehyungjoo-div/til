91807134814a3f25328da40095c0353022c5fb1a
leehyungjoo-div <juu1121@gmail.com>
2022-01-27 22:05:45 +0900

    python 문법 복습

---
+++ Python/python_ex2.py
@@ -0,0 +1,17 @@
+class post:
+    id = ''
+    title = ''
+    author = ''
+    content = ''
+
+str = post()
+str.id = 'master'
+str.title = '1'
+str.author = 'sparta'
+str.content = '숙제'
+
+print(str)
+print("id -> ", str.id)
+print("title -> ", str.title)
+print("author -> ", str.author)
+print("content -> ", str.content)
