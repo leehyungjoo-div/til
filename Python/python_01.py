b10ae3dc6675411cedcb211b6c1f2cd9535fda42
leehyungjoo-div <juu1121@gmail.com>
2022-01-27 22:07:09 +0900

    Revert "Revert "python 문법 복습""
    This reverts commit ef36403899d2fdb742365d802a0322da7b8adfed.

---
+++ Python/python_01.py
@@ -0,0 +1,41 @@
+#파이썬 복습
+
+a = 10
+b = 20
+
+sum_result = a+b
+mult_result = a*b
+div_result = a/b
+
+print(sum_result)
+print(mult_result)
+print(div_result)
+
+str_a = "Love"
+str_b = "Python"
+result = str_a + str_b
+print(result)
+
+a = "Love,eat,coding"
+split_result = a.split(",")
+print(split_result)
+
+my_list = [1,2,3,4,5]
+print(my_list)
+my_fav = ["python", "django", "web"]
+print(my_fav[0])
+print(my_fav[1])
+print(my_fav[2])
+my_fav.append("paul")
+my_fav.append("python")
+print(my_fav)
+
+my_player = {'name':'손흥민'}
+print(my_player)
+
+my_info = {'name':'paul','phone':'01012341234','birth':'0714'}
+print(my_info)
+print(my_info['name'])
+print(my_info.get('name'))
+
+
